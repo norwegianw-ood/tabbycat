09ddb6f0f64361c1ad955fdc42295bb7b58421ff
Étienne Beaulé <beauleetienne0@gmail.com>
2025-07-07 22:02:40 -0300

    Prepare for next 'T' release

--- docs/conf.py
+++ docs/conf.py
@@ -58,9 +58,9 @@ author = 'Philip Belesky, Chuan-Zheng Lee, Étienne Beaulé'
 # built documents.
 #
 # The short X.Y version.
-version = '2.10'
+version = '2.11'
 # The full version, including alpha/beta/rc tags.
-release = '2.10.0'
+release = '2.11.0-dev'
 
 rst_epilog = """
 .. |vrelease| replace:: v{release}
