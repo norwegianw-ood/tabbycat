89ff78b3c9edb642467fed2d93023f722a2d89bf
Benjamin Cao <edisonbenjcao@gmail.com>
2025-08-24 22:19:09 -0500

    Update wsgi.py

--- tabbycat/wsgi.py
+++ tabbycat/wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Point explicitly to production settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tabbycat.settings.production")
+# Use Render deployment settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tabbycat.settings.render")
 
 application = get_wsgi_application()
